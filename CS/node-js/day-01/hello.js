117cb5dbc7b9c9823fb86f287d28fcd27e5de642
pankajsharma81 <pankajsharma88279@gmail.com>
2025-04-11 00:13:25 +0530

    node-js Module func executor and file handling

---
+++ CS/node-js/day-01/hello.js
@@ -0,0 +1,8 @@
+const { addFunc, subFunc } = require("./math");
+
+// console.log(document)
+// console.log(alert);
+
+console.log("Hello world in nodejs");
+
+console.log(addFunc(2, 3) + " and " + subFunc(3, 2));
